ed3fe77861d92f7f322d1d24ab967fde7b501740
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-05-21 09:57:48 -0700

    twitter setup

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 require("dotenv").config();
-var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+
+//Twitter
+var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
+
+//Spotify
+var Spotify = require('node-spotify-api')
+var spotify = new Spotify(keys.spotify);
+
+
+//Omdb
+var request = require('request');
+
+//do-what it says
+var fs = require("fs");
+
+var params = {screen_name: 'realDonaldTrump'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+      for(var i = 0; i < 20; i++){
+        console.log("\n**********TWEET "+i+"**********"+tweets[i].text+" CREATED AT:",tweets[i].created_at);
+      }
+    
+  }
+});
