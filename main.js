cd549260e26dd72c2a5f7acc865fd69e86753b09
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-05-21 16:52:35 -0700

    Spotify doing

--- main.js
+++ main.js
@@ -16,12 +16,27 @@ var request = require('request');
 //do-what it says
 var fs = require("fs");
 
-var params = {screen_name: 'realDonaldTrump'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-      for(var i = 0; i < 20; i++){
-        console.log("\n**********TWEET "+i+"**********"+tweets[i].text+" CREATED AT:",tweets[i].created_at);
-      }
-    
-  }
-});
+//TWITTER
+// var params = {screen_name: 'realDonaldTrump'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//       for(var i = 0; i < 20; i++){
+//         console.log("\n**********TWEET "+i+"**********"+tweets[i].text+" CREATED AT:",tweets[i].created_at);
+//       }
+
+//   }
+// });
+
+//SPOTIFY
+// spotify.search({ type: 'track', query: 'Hello by Adele' }, function (err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+//   else {
+//     console.log("Artist Name:", data.tracks.items[0].album.artists[0].name);
+//     console.log("Song Name:", data.tracks.items[0].name);
+//     console.log("Preview URL:", data.tracks.items[0].preview_url);
+//     console.log("Album Name:", data.tracks.items[0].album.name);
+//   }
+
+// });
