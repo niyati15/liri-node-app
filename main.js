a76a764bde43bafa7c39b2744d4fbde4d5daa690
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-05-21 17:55:07 -0700

    inquirer code add

--- main.js
+++ main.js
@@ -19,68 +19,68 @@ var request = require('request');
 var fs = require("fs");
 
 //******************TWITTER******************
-// var params = {screen_name: 'realDonaldTrump'};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//       for(var i = 0; i < 20; i++){
-//         console.log("\n**********TWEET "+i+"**********"+tweets[i].text+" CREATED AT:",tweets[i].created_at);
-//       }
-
-//   }
-// });
+function findTweets(twitter_handle){
+  var params = {screen_name: twitter_handle};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        for(var i = 0; i < 20; i++){
+          console.log("\n**********TWEET "+i+"**********"+tweets[i].text+" CREATED AT:",tweets[i].created_at);
+        }
+  
+    }
+  });
+}
 
 //******************SPOTIFY******************
-// var song = "";
-// if (song) {
-//   findSongDetails(song);
-// } else {
-//   findSongDetails("The Sign by Ace of Base");
-// }
-
-// function findSongDetails(song) {
-//   spotify.search({ type: 'track', query: song }, function (err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     }
-//     else {
-//       console.log("Artist Name:", data.tracks.items[0].album.artists[0].name);
-//       console.log("Song Name:", data.tracks.items[0].name);
-//       console.log("Preview URL:", data.tracks.items[0].preview_url);
-//       console.log("Album Name:", data.tracks.items[0].album.name);
-//     }
-
-//   });
-// }
-
+function findSong(song){
+  if (song) {
+    findSongDetails(song);
+  } else {
+    findSongDetails("The Sign by Ace of Base");
+  }
+  
+  function findSongDetails(song) {
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      else {
+        console.log("Artist Name:", data.tracks.items[0].album.artists[0].name);
+        console.log("Song Name:", data.tracks.items[0].name);
+        console.log("Preview URL:", data.tracks.items[0].preview_url);
+        console.log("Album Name:", data.tracks.items[0].album.name);
+      }
+  
+    });
+  }
+}
 
 //******************OMDB******************
-// var movieName = "Moana";
-// if (movieName) {
-//   findMovieDetails(movieName);
-// } else {
-//   findMovieDetails("Mr. Nobody");
-// }
-// function findMovieDetails(movie) {
-//   var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-//   request(queryUrl, function (error, response, body) {
-
-
-//     if (!error && response.statusCode === 200) {
-//       console.log("Title: " + JSON.parse(body).Title);
-//       console.log("Year: " + JSON.parse(body).Year);
-//       console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);
-//       console.log("IMDB rating: " + JSON.parse(body).imdbRating);
-//       console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-//       console.log("language of the movie: " + JSON.parse(body).Language);
-//       console.log("Plot of the Movie: " + JSON.parse(body).Plot);
-//       console.log("Actors in the Movie: " + JSON.parse(body).Actors);
-//     }
-//   });
-// }
-
-
-
+function omdb(movieName){
+  if (movieName) {
+    findMovieDetails(movieName);
+  } else {
+    findMovieDetails("Mr. Nobody");
+  }
+  function findMovieDetails(movie) {
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  
+    request(queryUrl, function (error, response, body) {
+  
+  
+      if (!error && response.statusCode === 200) {
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Year: " + JSON.parse(body).Year);
+        console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+        console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+        console.log("language of the movie: " + JSON.parse(body).Language);
+        console.log("Plot of the Movie: " + JSON.parse(body).Plot);
+        console.log("Actors in the Movie: " + JSON.parse(body).Actors);
+      }
+    });
+  }
+}
 
 // Create a "Prompt" with a series of questions.
 inquirer
@@ -88,12 +88,29 @@ inquirer
     // Here we create a basic text prompt.
     {
       type: "input",
-      message: "You want to see the tweets of whom?",
+      message: "movie-this",
+      name: "movie"
+    },
+    {
+      type: "input",
+      message: "Spotify-this-song",
+      name: "song"
+    },
+    {
+      type: "input",
+      message: "Show me tweets by",
       name: "twitter"
     }
   ])
   .then(function(inquirerResponse) {
+    var movieName = inquirerResponse.movie;
+    omdb(movieName);
 
-    console.log(inquirerResponse);
+    var songName = inquirerResponse.song;
+    findSong(songName);
+
+    var twitterHandle = inquirerResponse.twitter;
+    findTweets(twitterHandle);
+
   });
 
