aa76f17aa8492b24c63abf4720618741f83ac439
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-05-22 09:01:18 -0700

    README.md added

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # liri-node-app
+
+In this command line app built with node, the terminal will take in various paramaters, hack the web, and bring that data back to you.
+
+## Getting Started :floppy_disk:
+
+### Prerequisites :open_file_folder:
+Make sure you have node.js installed on your computer! You can download it via the installer on the official site
+```
+https://nodejs.org/en/download/
+```
+Then either clone or download this repo.
+
+### Installation :file_folder:
+Inside your terminal or command prompt, navigate to the location of the cloned repo. Install the necessary dependencies by running - 
+```
+npm install
+```
+In order for the application to run certain commands you will need your own set of keys and tokens from Twitter and Spotify. Generate the keys via Twitter and Spotify's developer sites and store them in a `.env` file
+
+In your `.env` file replace the values with your API keys (no quotes):
+```
+# Spotify API keys
+
+SPOTIFY_ID=your-spotify-id
+SPOTIFY_SECRET=your-spotify-secret
+
+# Twitter API keys
+
+TWITTER_CONSUMER_KEY=your-twitter-consumer-key
+TWITTER_CONSUMER_SECRET=your-twitter-consumer-secret
+TWITTER_ACCESS_TOKEN_KEY=your-access-token-key
+TWITTER_ACCESS_TOKEN_SECRET=your-twitter-access-token-secret
+```
+
+## Using the App :computer:
+
+Type `node main.js` into your terminal to begin using the app. You can navigate through the commands using the up and down arrows and hitting enter. 
+
+
+## Built With :cd:
+* [Javascript](https://www.javascript.com/) - programming language
+* [Node.js](https://nodejs.org/en/) - javascript runtime
+
+### NPM Packages
+* [Twitter](https://www.npmjs.com/package/twitter)
+* [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+* [request](https://www.npmjs.com/package/request)
+* [dotenv](https://www.npmjs.com/package/dotenv)
+* [inquirer](https://www.npmjs.com/package/inquirer)
+
+## Author :key:
+* **Niyati Shah** - [niyati15](https://github.com/niyati15)
+
+
+## Acknowledgments :pray:
+A big thank you to our instructor, Jerome, and the TAs, Amber, Ricky, and Sasha!!
--- main.js
+++ main.js
@@ -12,6 +12,9 @@ var spotify = new Spotify(keys.spotify);
 //Inquirer
 var inquirer = require("inquirer");
 
+//Inquirer-recursive
+var inquirerRecursive = require("inquirer-recursive");
+
 //Omdb
 var request = require('request');
 
@@ -19,26 +22,26 @@ var request = require('request');
 var fs = require("fs");
 
 //******************TWITTER******************
-function findTweets(twitter_handle){
-  var params = {screen_name: twitter_handle};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+function findTweets(twitter_handle) {
+  var params = { screen_name: twitter_handle };
+  client.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
-        for(var i = 0; i < 20; i++){
-          console.log("\n**********TWEET "+i+"**********"+tweets[i].text+" CREATED AT:",tweets[i].created_at);
-        }
-  
+      for (var i = 0; i < 20; i++) {
+        console.log("\n**********TWEET " + i + "**********" + tweets[i].text + " CREATED AT:", tweets[i].created_at);
+      }
+
     }
   });
 }
 
 //******************SPOTIFY******************
-function findSong(song){
+function findSong(song) {
   if (song) {
     findSongDetails(song);
   } else {
     findSongDetails("The Sign by Ace of Base");
   }
-  
+
   function findSongDetails(song) {
     spotify.search({ type: 'track', query: song }, function (err, data) {
       if (err) {
@@ -50,13 +53,13 @@ function findSong(song){
         console.log("Preview URL:", data.tracks.items[0].preview_url);
         console.log("Album Name:", data.tracks.items[0].album.name);
       }
-  
+
     });
   }
 }
 
 //******************OMDB******************
-function omdb(movieName){
+function omdb(movieName) {
   if (movieName) {
     findMovieDetails(movieName);
   } else {
@@ -64,10 +67,10 @@ function omdb(movieName){
   }
   function findMovieDetails(movie) {
     var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-  
+
     request(queryUrl, function (error, response, body) {
-  
-  
+
+
       if (!error && response.statusCode === 200) {
         console.log("Title: " + JSON.parse(body).Title);
         console.log("Year: " + JSON.parse(body).Year);
@@ -83,34 +86,62 @@ function omdb(movieName){
 }
 
 // Create a "Prompt" with a series of questions.
-inquirer
-  .prompt([
-    // Here we create a basic text prompt.
-    {
-      type: "input",
-      message: "movie-this",
-      name: "movie"
-    },
-    {
-      type: "input",
-      message: "Spotify-this-song",
-      name: "song"
-    },
-    {
-      type: "input",
-      message: "Show me tweets by",
-      name: "twitter"
-    }
-  ])
-  .then(function(inquirerResponse) {
-    var movieName = inquirerResponse.movie;
-    omdb(movieName);
+inquirer.registerPrompt('recursive', require('inquirer-recursive'));
+inquirer.prompt([{
+  type: 'recursive',
+  message: 'Add a new user ?',
+  name: 'users',
+  prompts: [
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is user\'s name?',
+          validate: function (value) {
+              if ((/.+/).test(value)) { return true; }
+              return 'name is required';
+          }
+      }, {
+          type: 'input',
+          name: 'age',
+          message: 'How old is he?',
+          validate: function (value) {
+              var digitsOnly = /\d+/;
+              if (digitsOnly.test(value)) { return true; }
+              return 'Invalid age! Must be a number genius!';
+          }
+      }
+  ]
+}]).then(function(answers) {
+  console.log(answers.users);
+  /*
+  OUTPUT :
+  [
+      {
+          name: 'Brendan Eich',
+          age: '42',
+      }, {
+          name: 'Jordan Walke',
+          age: '13',
+      },
+      ...
+  ]
+  */
+});
+  // .then(function (inquirerResponse) {
+  //   var choice = inquirerResponse.choices;
+  //   switch (choices) {
+  //     case "Movie-this":
+  //       omdb(choice);
+  //       break;
+  //     case "Spotify-this":
+  //       findSong(choice);
+  //       break;
+  //     case "Find-tweets":
+  //       findTweets(choice);
+  //       break;
+  //   }
+  // });+
 
-    var songName = inquirerResponse.song;
-    findSong(songName);
 
-    var twitterHandle = inquirerResponse.twitter;
-    findTweets(twitterHandle);
 
-  });
 
